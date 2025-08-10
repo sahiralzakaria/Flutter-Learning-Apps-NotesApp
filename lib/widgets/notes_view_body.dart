29516b507b44c19d4f931e307bfbb24a38f27fbf
SAHİR ALZAKARIA <sahir.alzakaria@gmail.com>
2025-08-10 18:59:11 +0300

    create edit note view

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -9,7 +9,7 @@ class NotesViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
           SizedBox(height: 50),
