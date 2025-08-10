7872fa0bb0f38fa1ea928bc35c8d42eaae6108fe
SAHİR ALZAKARIA <sahir.alzakaria@gmail.com>
2025-08-10 16:49:26 +0300

    Edit text field

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -6,6 +6,9 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [SizedBox(height: 24), CustomTextField()]);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(children: [SizedBox(height: 24), CustomTextField()]),
+    );
   }
 }
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/constants.dart';
 
 class CustomTextField extends StatelessWidget {
   const CustomTextField({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return TextField(decoration: InputDecoration(border: OutlineInputBorder()));
+    return TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+        hintText: 'Title',
+        hintStyle: TextStyle(color: kPrimaryColor),
+        border: buildBorder(),
+        focusedBorder: buildBorder(kPrimaryColor),
+        enabledBorder: buildBorder(),
+      ),
+    );
   }
+
+  OutlineInputBorder buildBorder([color]) => OutlineInputBorder(
+    borderRadius: BorderRadius.circular(8),
+    borderSide: BorderSide(color: color ?? Colors.white),
+  );
 }
