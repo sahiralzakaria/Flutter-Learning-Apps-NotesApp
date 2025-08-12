e2bb7c879d47a4851ff520e0469141333ebcce10
SAHİR ALZAKARIA <sahir.alzakaria@gmail.com>
2025-08-13 02:13:28 +0300

    fix some bugs

--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -5,14 +5,13 @@ import 'package:noteapp/models/note_model.dart';
 import 'package:noteapp/widgets/custom_note_item.dart';
 
 class NotesListView extends StatelessWidget {
-  const NotesListView({super.key});
+  const NotesListView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<NotesCubit, NotesState>(
       builder: (context, state) {
-        List<NoteModel> notes =
-            BlocProvider.of<NotesCubit>(context).notes ?? [];
+        List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes!;
         return Padding(
           padding: const EdgeInsets.symmetric(vertical: 16),
           child: ListView.builder(
